07090891d33ce914439006a526922e01e94fa01e
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-06-25 15:37:16 -0700

    added based files in main branch

---
+++ components/MiguelHi.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <div>
+        hi
+    </div>
+</template>
+<script>
+export default {
+    name:"MiguelHi"
+}
+</script>
+<style>
+    
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <MiguelHi/>
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
