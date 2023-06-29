5bb9da027c91a778545225c74e1829e9897d79fd
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-06-28 20:55:32 -0700

    video fixed

--- components/About.vue
+++ components/About.vue
@@ -1,32 +1,48 @@
 <template>
-    <div>
+    <div class="about">
         <video src="~/assets/video/bg-video.mp4" loop muted autoplay></video>
         <div class="vid-opc">
 
         </div>
         <div class="about-content">
             <div class="about-container">
-                <figure>
-                    <h3>Wink Content</h3>
-                    <IconsWink/>
-                    <figcaption>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam doloremque rem reiciendis dolor corporis, blanditiis neque temporibus. Sequi, odio nisi dolorem doloremque architecto at vel earum ratione commodi autem.
-                    </figcaption>
-                </figure>
-                <figure>
-                    <h3>Wink Content</h3>
-                    <IconsWink/>
-                    <figcaption>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam doloremque rem reiciendis dolor corporis, blanditiis neque temporibus. Sequi, odio nisi dolorem doloremque architecto at vel earum ratione commodi autem.
-                    </figcaption>
-                </figure>
-                <figure>
-                    <h3>Wink Content</h3>
-                    <IconsWink/>
-                    <figcaption>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam doloremque rem reiciendis dolor corporis, blanditiis neque temporibus. Sequi, odio nisi dolorem doloremque architecto at vel earum ratione commodi autem.
-                    </figcaption>
-                </figure>
+                <article>
+                    <div class="fig-opc">
+
+                    </div>
+                    <figure>
+                        <h3>Wink Content</h3>
+                        <IconsWink/>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam doloremque rem reiciendis dolor corporis, blanditiis neque temporibus. Sequi, odio nisi dolorem doloremque architecto at vel earum ratione commodi autem.
+                        </figcaption>
+                    </figure>
+                </article>
+                
+                <article>
+                    <div class="fig-opc">
+                        
+                    </div>
+                    <figure>
+                        <h3>Wink Content</h3>
+                        <IconsWink/>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam doloremque rem reiciendis dolor corporis, blanditiis neque temporibus. Sequi, odio nisi dolorem doloremque architecto at vel earum ratione commodi autem.
+                        </figcaption>
+                    </figure>
+                </article>
+                <article>
+                    <div class="fig-opc">
+                        
+                    </div>
+                    <figure>
+                        <h3>Wink Content</h3>
+                        <IconsWink/>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam doloremque rem reiciendis dolor corporis, blanditiis neque temporibus. Sequi, odio nisi dolorem doloremque architecto at vel earum ratione commodi autem.
+                        </figcaption>
+                    </figure>
+                </article>
             </div>
         </div>
         
@@ -35,7 +51,40 @@
 
 <style>
     video{
+        height: 100%;
         width: 100%;
-        height: 30vh;
+        object-fit: cover;
+        object-position: center;
+        position: absolute;
+        z-index: 1;
     }
+
+    .about{
+        height: 65vh;
+        position: relative;
+    }
+
+    .vid-opc{
+        background-color: lightblue;
+        opacity: .2;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        z-index: 2;
+    }
+
+    .about-content{
+        justify-content: end;
+        position: absolute;
+        z-index: 3;
+        width: 100%;
+        height: 100%;
+    }
+
+    .about-container{
+        display: flex;
+    }
+    
+
+
 </style>
