f21194a585b4f0447435d31d030a98b336eaddca
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-07-06 17:15:26 -0700

    conflict fixed

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
-    
+  <div class="mainView" v-if="page === 0">
+    <HeaderPage />
+    <About />
+    <!-- intro page -->
+    <main class="cont">
+      <h1>One piece MBTI</h1>
+      <Button text="Start test" :clickEvent="startEvent" />
+    </main>
+    <Main />
+    <FooterPage />
   </div>
+  <div class="mainView" v-else-if="page < 13">
+    <!-- quiz page -->
+    <main class="mainQuestion">
+      <Question />
+    </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  
 }
 </script>
