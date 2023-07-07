374c6d84e0b967686d67143fe0c868fe97196bec
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-07-06 18:16:06 -0700

    merge conflict fixed

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,61 @@
 <template>
-  <div>
-    
+  <div class="mainView" v-if="page === 0">
+    <HeaderPage />
+    <About />
+    <!-- intro page -->
+    <main class="cont">
+      <h1>One piece MBTI</h1>
+      <Button text="Start test" :clickEvent="startEvent" />
+    </main>
+    <Main />
+    <FooterPage />
+  </div>
+  <div class="mainView" v-else-if="page < 13">
+    <!-- quiz page -->
+    <main class="mainQuestion">
+      <Question />
+    </main>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  computed: {
+    page() {
+      return this.$store.state.page;
+    },
+  },
+  methods: {
+    startEvent() {
+      this.$store.commit("SET_PAGE", 1);
+    },
+  },
+};
 </script>
+<style scoped>
+.mainView {
+  display: flex;
+  flex-direction: column;
+}
+.mainQuestion {
+  width: 100%;
+  height: 100vh;
+}
+
+.cont{
+    height: 65vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-top: 2px solid grey;
+    background:linear-gradient(180deg,whitesmoke ,#FFD0D0); 
+    flex-direction: column;
+}
+
+.cont > h1{
+    padding-bottom: 4vh;
+    text-transform: uppercase;
+    color: #164B60;
+}
+</style>
+
